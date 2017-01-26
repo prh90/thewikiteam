1b9b84b3898ac64fb2f59f82a30e8f2924582ed3
Sean Wright & Pablo R. Hernandez <pair+nightangelblade.prh90@devbootcamp.com>
2017-01-26 14:21:41 -0800

    Got footer button working.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,7 +11,7 @@ class ArticlesController < ApplicationController
     p article_params
     # p categories
     # creator_id: current_user.id, Needed to complete submission
-    @article = Article.new(title: article_params[:title], summary: article_params[:summary])
+    @article = Article.new(creator_id: 1, title: article_params[:title], summary: article_params[:summary])
     if @article.save
       Category.find(related_categories).each {|category| category.articles << @article}
       redirect_to article_path(@article)
