410ba465ea3e9d504d3c276896decfff5709046b
cclazarou <candacelazarou@gmail.com>
2017-01-26 09:14:45 -0800

    Add user registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,15 @@
 class User < ApplicationRecord
+
   has_secure_password
 
   validates :username, presence: true
   validates :email, presence: true, uniqueness: true
-  validates :password_digest, presence: true
+  validates :password, presence: true
+
 
   has_many :created_articles, class_name: 'Article', foreign_key: "creator_id"
   # revision is a table of logs when a section is changed
   has_many :revisions
+  # a revised section is a section that has been revised
   has_many :revised_sections, through: :revisions, source: :section
 end
